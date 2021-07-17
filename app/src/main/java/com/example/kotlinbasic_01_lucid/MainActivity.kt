f8e814a4d03b4e23a0d72a5049c83f7f49171a12
windows <nvpz1598@gmail.com>
2021-07-17 10:14:03 +0900

    로그 찍어보기

--- app/src/main/java/com/example/kotlinbasic_01_lucid/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic_01_lucid/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlinbasic_01_lucid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        clickBtn
+        clickBtn.setOnClickListener {
+
+            //코드에 메모 남기기 - 주석
+//            ctrl + / : 커서가 있는 줄을 주석 처리.
+//            이 {  } 안에 있는 코드만, 버튼이 눌렸을 때 실행됨.
+
+            Log.d("메인화면로그","클릭용 버튼 눌림")
+
+        }
 
         smallbBtn
 
