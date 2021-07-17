027e5802e81b19b945beced75728a8f4163e7715
windows <nvpz1598@gmail.com>
2021-07-17 10:02:03 +0900

    버튼 2개 추가 - 기초 이벤트에 사용 예정

--- app/src/main/java/com/example/kotlinbasic_01_lucid/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic_01_lucid/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.kotlinbasic_01_lucid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clickBtn
+
+        smallbBtn
+
     }
 }
